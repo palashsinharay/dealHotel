147948108c1b8c73c1cc0f52894dd1347091f705
bunty <sahani.bunty9@gmail.com>
2013-08-26 16:05:00 +0530

    recently booked hotels by country

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -3,7 +3,8 @@
 class Test extends CI_Controller{
     
     public function index() {
-         echo  ConvertCurrency(1, 'USD', 'INR');  
+         //echo  ConvertCurrency(1, 'USD', 'INR');
+        getCountryName();
     }
     
    
